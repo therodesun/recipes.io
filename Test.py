e9e3d420e7b5c7102831aa377c2c9352f4a8f315
Mankler <matt_rey@me.com>
2021-02-19 14:54:13 -0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,5 +5,8 @@ class Testing(unittest.TestCase):
     def test_blank(self):
         pass
     
+    def test_fail(self):
+        self.assertTrue(False)
+    
 if __name__ == '__main__':
     unittest.main()
