895868b6da245e1e8ab020894e316e2c04e8c820
Matthew Reynolds <mreyno29@calpoly.edu>
2021-03-01 13:13:33 -0800

    adjusted css for homepage

--- ui/src/Home/Carousel.js
+++ ui/src/Home/Carousel.js
@@ -7,14 +7,18 @@ class Carousel extends Component {
     render() { 
         return ( 
             <div>
-                <h2>
+                <h2 id = "recommend">
                     Recommended Recipes
                 </h2>
-                <Button class = "carousel_button" variant="btn btn-success" onClick={() => history.push('/RecipePage')}>
-                    <img src = "https://c.ndtvimg.com/2020-01/n7thfo2o_spaghetti_625x300_28_January_20.jpg" id="image" height = {300} width = {300}></img>
+                <Button  variant="btn btn-success" onClick={() => history.push('/RecipePage')}>
+                    <div id = "carousel">
+                        <img class = "class" alt = '' src = "https://c.ndtvimg.com/2020-01/n7thfo2o_spaghetti_625x300_28_January_20.jpg" id="image"></img>
+                    </div>
                 </Button>
-                <Button class = "carousel_button" variant="btn btn-success" onClick={() => history.push('/RecipePage')}>
-                    <img src = "https://static.toiimg.com/photo/53110049.cms" id="image" height = {300} width = {300}></img>
+                <Button  variant="btn btn-success" onClick={() => history.push('/RecipePage')}>
+                    <div id = "carousel">
+                        <img class = "class" alt = '' src = "https://static.toiimg.com/photo/53110049.cms" id="image"></img>
+                    </div>
                 </Button>
             </div>
          );
