61798952983b24524408e1e4d296c45ae445697d
John <therodesup@hotmail.com>
2021-03-07 05:59:32 -0800

    some testing layout

--- backend/testing.py
+++ backend/testing.py
@@ -4,23 +4,36 @@ import flask_unittest
 from recipe import app
 
 class TestStringMethods(unittest.TestCase):
-    def test_index(self):
+    # test for printing recipes list
+    def test_show_recipes(self):
         tester = app.test_client(self)
         response = tester.get('/recipes')
         self.assertEqual(response.status_code,200)
     
+    # test for post recipe list 
+    def test_add_recipe(self):
+        tester = app.test_client(self)
+        response = tester.post('/recipes',json={"name":"egg","time":"15 min"})
+        self.assertEqual(response.status_code,201)
+    
     # Testing if get returns the right page
+    # need to post the data first
     def test_find_recipe(self):
         tester = app.test_client(self)
         response = tester.get('/recipes/Roasted_Asparagus')
         self.assertEqual(response.status_code, 200)
+    
 
-    # Testing proper error code
+    # Testing proper error code 
     def test_no_Recipe(self):
         tester = app.test_client(self)
         response = tester.get('/recipes/nonexistentrecipe1213123123123')
         self.assertEqual(response.status_code, 404)
-    
+    # Test delete by name 
+    def test_delete_name(self):
+        tester = app.test_client(self)
+        response = tester.delete('/recipes/egg')
+        self.assertEqual(response.status_code, 200)
     # Testing update recipe
     def test_update(self):
         tester = app.test_client(self)
@@ -55,17 +68,45 @@ class TestStringMethods(unittest.TestCase):
             ],
             "imageURL": "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/50/84/7/picMcSyVd.jpg",
             "originalURL": "http://www.food.com/recipe/roasted-asparagus-50847",
-            "response":true
+            "response":"true"
         })
         self.assertEqual(response.status_code, 201)
 
     # Testing update recipe error
-    def test_update(self):
+    def test_update_2(self):
         tester = app.test_client(self)
         response = tester.post('/update', json=None)
         self.assertEqual(response.status_code, 400)
+
+    # test get curret
+    def test_current(self):
+        pass
+   
+    #test my recipes
+    def test_myrecipe_get(self):
+        pass
+    def test_myrecipe_get_fail(self):
+        pass
+    def test_myrecipe_post(self):
+        pass
+    def test_myrecipe_post_fail(self):
+        pass
+    def test_my_recipe_delete(self):
+        pass
+
+    #test shopping 
     
-    
+    def test_shoppig_get(self):
+        pass
+    def test_shoppig_get_fail(self):
+        pass
+    def test_shoppig_post(self):
+        pass
+    def test_shoppig_post_fail(self):
+        pass
+    def test_my_shoppig_delete(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
