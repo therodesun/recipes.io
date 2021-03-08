7cc25a6e1030832b785b6980a77acf11502b55c4
John <therodesup@hotmail.com>
2021-03-07 19:08:16 -0800

    add logo

--- ui/src/Home/Home.js
+++ ui/src/Home/Home.js
@@ -2,17 +2,28 @@ import React, { Component } from "react";
 import "./Home.css";
 import Search from "./Search.js"
 import Carousel from "./Carousel.js"
-
+import img from "./images/recipe.png"
 export default class Home extends Component {
+  
   render() {
+    
     return (
       <div className="Home">
         <div className="lander">
           <h1 id="titleRecip">Welcome to Reci.p!</h1>
+         
             <Search/>
             <Carousel/>
         </div>
+        <div id = "image">
+        <img 
+        src= {img} 
+        alt="new"
+      />
+        </div>
+       
       </div>
+      
     );
   }
 }
